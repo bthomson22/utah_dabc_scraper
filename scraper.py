d26a9a9f4d9461313c1f2447793b1e14879ac307
bthomson22 <brandon@penpath.com>
2021-07-23 01:59:05 -0600

    Added email functionality

--- scraper.py
+++ scraper.py
@@ -1,87 +1,150 @@
-# Import necessary packages
 from bs4 import BeautifulSoup
-import requests
 import pandas as pd
+import requests
+import smtplib
+import logging
 import re
+from pretty_html_table import build_table
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-pd.set_option("expand_frame_repr", True)
-# Site URL
 
-products_dict = {'Crater Lake':'910543','Vida':'089221','Larceny':'018856'}
-appended_data = []
-# headings = ('Store','Name','Address','City','Phone','Store Qty','Pin')
+subject = 'Booze available!'
 
-for key, value in products_dict.items():
+def get_credentials():
+    credentials = {}
+    with open('passwords.txt') as f:
+        for x in f.readlines():
+            entry = x.strip().split(':')
+            credentials[entry[0]] = entry[1]
+    return credentials
 
-    url= f'https://webapps2.abc.utah.gov/ProdApps/ProductLocatorCore/ProductDetail/Index?sku={value}'
+def get_mail_list():
+    with open('mail_list.txt') as f:
+        mail_list = [x.strip().split(', ') for x in f.readlines()][0]
+    return mail_list
+        
+def setup_logging():
+    logging.basicConfig(filename='scrape.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
 
-    # Make a GET request to fetch the raw HTML content
-    html_content = requests.get(url).text
+def main():
+    credentials = get_credentials()
+    mail_list = get_mail_list()
+    products_dict = {'Crater Lake':'910543','Vida':'089221','Larceny':'018856'}
+    appended_data = []  
+    setup_logging()
+    
+    try:
+        for key, value in products_dict.items():
 
-    # Parse HTML code for the entire site
-    soup = BeautifulSoup(html_content, "lxml")
-    # print(soup.prettify()) # print the parsed data of html
+            url= f'https://webapps2.abc.utah.gov/ProdApps/ProductLocatorCore/ProductDetail/Index?sku={value}'
+            html_content = requests.get(url).text
+            soup = BeautifulSoup(html_content, "lxml")
+            gdp = soup.find_all("table", id = "storeTable")
+            table1 = gdp[0]
+            body = table1.find_all("tr")
+            head = body[0]
+            body_rows = body[1:]
 
-    # On site there are 3 tables with the class "wikitable"
-    # The following line will generate a list of HTML content for each table
-    gdp = soup.find_all("table", id = "storeTable")
-    print("Number of tables on site: ",len(gdp))
+            headings = ('Store','Name','Address','City','Phone','Store Qty','Pin')
+        
+            all_rows = []
+            for row_num in range(len(body_rows)):
+                row = []
+                for row_item in body_rows[row_num].find_all("td"):
+                    aa = re.sub("(\xa0)|(\n)|,","",row_item.text)
+                    row.append(aa)
+                all_rows.append(row)
 
-    # Lets go ahead and scrape first table with HTML code gdp[0]
-    table1 = gdp[0]
-    # the head will form our column names
-    body = table1.find_all("tr")
-    # Head values (Column names) are the first items of the body list
-    head = body[0] # 0th item is the header row
-    body_rows = body[1:] # All other items becomes the rest of the rows
+            df_prod = pd.DataFrame(data=all_rows, columns=headings)
+            df_prod['Product Name'] = key
+            df_prod['Product_ID'] = value
+            # print(df_prod)
+            appended_data.append(df_prod)
 
-    # Lets now iterate through the head HTML code and make list of clean headings
+        # print('\n########## ALL STORES ##########\n')
 
-    # Declare empty list to keep Columns names
-    headings = ('Store','Name','Address','City','Phone','Store Qty','Pin')
-    # for item in head.find_all("th"): # loop through all th elements
-    #     # convert the th elements to text and strip "\n"
-    #     item = (item.text).rstrip("\n")
-    #     # append the clean column name to headings
-    #     headings.append(item)
-    # print(headings)
+        df = pd.concat(appended_data, ignore_index = True)
+        # print(df)
 
-    # Next is now to loop though the rest of the rows
+        df['Store Qty'] = df['Store Qty'].astype(int)
+        df = df[df['Store Qty'] > 0]
+        print(df)
 
-    #print(body_rows[0])
- # will be a list for list for all rows
-    all_rows = []
-    for row_num in range(len(body_rows)): # A row at a time
-        row = [] # this will old entries for one row
-        for row_item in body_rows[row_num].find_all("td"): #loop through all row entries
-            # row_item.text removes the tags from the entries
-            # the following regex is to remove \xa0 and \n and comma from row_item.text
-            # xa0 encodes the flag, \n is the newline and comma separates thousands in numbers
-            aa = re.sub("(\xa0)|(\n)|,","",row_item.text)
-            #append aa to row - note one row entry is being appended
-            row.append(aa)
-        # append one row to all_rows
-        all_rows.append(row)
+        if len(df.index)>0:
+            try:
+                gmail_user = credentials['username']
+                gmail_password = credentials['password']
+                sent_from = gmail_user
+                to = mail_list
+                # subject = 'Booze available!'
+                # msg_body = build_table(df,'blue_light')
+                # message = """From: %s
+                # To: %s
+                # Subject: %s
+                # %s
+                # """ % (sent_from, to, subject, msg_body)
 
-    df_prod = pd.DataFrame(data=all_rows, columns=headings)
-    df_prod['Product Name'] = key
-    df_prod['Product_ID'] = value
-    # print(df_prod)
-    appended_data.append(df_prod)
+                # server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                # server.ehlo()
+                # server.login(gmail_user, gmail_password)
+                # server.sendmail(sent_from, to, message)
+                # server.close()
 
-# We can now use the data on all_rowsa and headings to make a table
-# all_rows becomes our data and headings the column names
-print('\n########## ALL STORES ##########\n')
+                message = MIMEMultipart()
+                message['Subject'] = 'Booze Available!'
+                message['From'] = 'utah.dabc.scraper@gmail.com'
+                message['To'] = 'bthomson22@gmail.com'
+                body_content = build_table(df, 'blue_light')
+                message.attach(MIMEText(body_content, 'html'))
+                msg_body = message.as_string()
+                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server.ehlo()
+                server.login(gmail_user, gmail_password)
+                server.sendmail(message['From'], message['To'], msg_body)
+                server.close()
+                
+                logging.info('Email sent!')
+                print("\nThere's some available!")
+                # print(df)
 
-df = pd.concat(appended_data, ignore_index = True)
-# print(df)
+            # except SMTPResponseException as e:
+            #     error_code = e.smtp_code
+            #     error_message = e.smtp_error
 
-df['Store Qty'] = df['Store Qty'].astype(int)
+            except Exception:
+                logging.info('Something went wrong...')
 
-df = df[df['Store Qty'] > 0]
+        else:
+            logging.info('None available :(')
+            print("\nNone available :(")
 
-if len(df.index)>0:
-    print("\nThere's some available!")
-    print(df)
-else:
-    print("\nNone available :(")
+    except Exception:
+        logging.info('Failed to scrape')
+
+def check_logger():
+    try:
+        infile = r"scrape.log"
+        important = []
+        keep_phrases = ["Email sent!"]
+        with open(infile) as f:
+            f = f.readlines()
+        for line in f:
+            for phrase in keep_phrases:
+                if phrase in line:
+                    important.append(line)
+                    break
+        if len(important) > 0:
+            return "Stop"
+        else:
+            return "Go"
+    except Exception:
+        return "Go"
+
+if __name__ == '__main__':
+    main()
